4bccd84c9a7674936bd42c8ec47971689a8b699e
Fausto <faustocamp545@gmail.com>
2022-07-28 18:57:22 -0500

    Development until ServiceImpl

---
+++ src/main/java/com/example/cursospringboot/entity/User.java
@@ -0,0 +1,46 @@
+package com.example.cursospringboot.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "users")
+public class User implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(length = 50)
+	private String name;
+	
+	private String surname;
+	
+	@Column(nullable = false, length = 50, unique = true)
+	private String email;
+	
+	private Boolean enabled;
+	
+	
+	
+	
+
+}
